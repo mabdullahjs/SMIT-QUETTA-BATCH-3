1725c0b6ff5763116939e09c4cecf5b93aa3f5ce
mabdullahjs <mabdullah2037@outlook.com>
2025-11-22 12:57:55 +0500

    object started

---
+++ 02-Javascript/12-Objects/object.js
@@ -0,0 +1,93 @@
+// let arr = [""]
+
+// // collection of key and properties is called object
+// let user = {
+//     fullname: "Muhammad Abdullah",
+//     email: "mabdullah@gmail.com",
+//     age: 22,
+//     hobbies: ['cricket' , 'swimming' , 'eating'],
+//     greetUser: function (){
+//      console.log("hello " + this.fullname);
+//      console.log(this);
+
+//     },
+//     isLoggedIn: true
+// }
+
+// console.log(user.age);
+// console.log(user["fullname"]);
+
+// console.log(user.isLoggedIn);
+// user.greetUser()
+
+// THIS
+
+// console.log(this);
+// window.alert()
+// window.console.log()
+
+// const companyInfo = {
+//   name: "innovatrix tech",
+//   founder: "ABD",
+//   since: 2022,
+//   members: ["hasnain", "rumman"],
+// };
+
+// // companyInfo = "testing"
+// // update
+// companyInfo.name = "Devistiq"
+
+// // add
+// companyInfo.newProp = "New property"
+
+// // delete
+// delete companyInfo.since
+
+// console.log(companyInfo);
+
+
+
+
+
+
+// const user = {
+//     name: "abdullah",
+//     greetUser: function (){
+//         return `hello ${this.name}`
+//     }
+// }
+
+// console.log(user.greetUser());
+
+
+
+// const users = [
+//   {
+//     id: 1,
+//     name: "Alice Johnson",
+//     email: "alice@example.com",
+//     age: 25
+//   },
+//   {
+//     id: 2,
+//     name: "Bob Smith",
+//     email: "bob@example.com",
+//     age: 30
+//   },
+//   {
+//     id: 3,
+//     name: "Charlie Davis",
+//     email: "charlie@example.com",
+//     age: 28
+//   }
+// ];
+
+// console.log(users[1].name);
+
+
+
+
+//obj banana haa name,email,age,greetuser uska baad firstname and lastname baad ma add krna haa or name delete krna ha. greet user ka function jab bhi call hoo to wo return kraa hello '${name}'
+
+// 4 formfields banaingi name , email , age, city jaisa hi user submit pa click kraiga mera pass console ma aik object ajayega jiska undar values form wali hongi
+
