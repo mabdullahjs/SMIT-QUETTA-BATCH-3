1b984c48002252b67d2376ed18d6088159c46e3c
mabdullahjs <mabdullah2037@outlook.com>
2026-01-11 13:28:00 +0500

    react started

---
+++ 03-React-JS/01-introduction/src/App.jsx
@@ -0,0 +1,78 @@
+// import About from "./About"
+// import Test from "./Test"
+
+// function App() {
+//   return (
+//     <>
+//       <h1>Hello from App</h1>
+//       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae voluptatem, similique deleniti esse ex quisquam dicta nam tempora dolor, adipisci non. Fugit fuga perferendis veniam maiores architecto eum alias ducimus!</p>
+//       <Test/>
+//       <About/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+// import React from 'react'
+
+// const App = () => {
+//   const username = "mabdullah";
+//   return (
+//     <>
+//       <h1>{username} Hello {20 + 20} App {username}</h1>
+//       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. {username} unde ratione ipsa fugiat assumenda voluptatibus impedit nam accusamus {username} Ab facilis incidunt aspernatur eveniet odit, sint accusantium {username} quisquam autem?</p>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+// import React, { useState } from 'react'
+
+// const App = () => {
+//   // let num = 0
+//   // hook
+//   const [num , setNum] = useState(0)
+
+//   const addCounter = ()=>{
+//     setNum(num + 1)
+//     // num = 10;
+//     console.log("add counter " , num);
+    
+//   }
+//   const lessCounter = ()=>{
+//     // num -= 1
+//     num === 0 ? alert('kiya chah rha ha') : setNum(num - 1)
+//     console.log("less counter " , num);
+    
+//   }
+//   return (
+//     <>
+//     <h1>Hello world {num}</h1>
+//     <button onClick={addCounter}>+</button>
+//     <button onClick={lessCounter}>-</button>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+// Todo app
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ 03-React-JS/01-introduction/src/Test.jsx
@@ -0,0 +1,7 @@
+function Test(){
+    return (
+        <h1>Test component</h1>
+    )
+}
+
+export default Test
