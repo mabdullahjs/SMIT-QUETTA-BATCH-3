98f2e895ea68e613fe825743f97145c0b9d42c50
mabdullahjs <mabdullah2037@outlook.com>
2026-01-25 13:04:03 +0500

    api call

--- 03-React-JS/03-Quiz-App/src/App.jsx
+++ 03-React-JS/03-Quiz-App/src/App.jsx
@@ -1,14 +1,61 @@
-// import { useState } from "react"
-// import Btn from "./components/Btn"
+// // import { useState } from "react"
+// // import Btn from "./components/Btn"
 
-// function App(){
-//   const [mount , setMount] = useState(true)
+// // function App(){
+// //   const [mount , setMount] = useState(true)
+// //   return (
+// //     <>
+// //     <h1>Hello world</h1>
+// //     <button onClick={()=>setMount(!mount)}>Button component {mount ? 'unmount' : 'mount'}</button>
+// //     {/* {mount ? <Btn/> : null} */}
+// //     {mount && <Btn/>}
+// //     </>
+// //   )
+// // }
+
+// // export default App
+
+
+// // // use effect
+
+
+
+// import React, { useEffect, useState } from 'react'
+
+// const App = () => {
+//   const [users , setUsers] = useState(null);
+//   const [loading , setLoading] = useState(true);
+//   const [error , setError] = useState(false);
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then(res => res.json())
+//       .then(res => {
+//         console.log(res);
+//         setUsers(res)
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         setError(true)
+
+//       }).finally(()=>{
+//         setLoading(false)
+//       })
+//   }, [])
 //   return (
 //     <>
-//     <h1>Hello world</h1>
-//     <button onClick={()=>setMount(!mount)}>Button component {mount ? 'unmount' : 'mount'}</button>
-//     {/* {mount ? <Btn/> : null} */}
-//     {mount && <Btn/>}
+//       <h1>Hello world</h1>
+//       {loading && <h1>Loading...</h1>}
+//       {error && <h1>Error occured.</h1>}
+//       {users && users.map(item => {
+//         return <h3 key={item.id}>{item.name}</h3>
+//       })}
+// {/* 
+//       {loading ? <h1>Loading...</h1> : null}
+//       {error ? <h1>Error occured.</h1> : null}
+//       {users ? users.map(item => {
+//         return <h3 key={item.id}>{item.name}</h3>
+//       }) : null} */}
+
 //     </>
 //   )
 // }
@@ -16,53 +63,49 @@
 // export default App
 
 
-// // use effect
+// // falsy values in js
+
+// // '' , 0 , null , undefined , false
 
 
 
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [users , setUsers] = useState(null);
-  const [loading , setLoading] = useState(true);
-  const [error , setError] = useState(false);
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://fakestoreapi.com/products')
       .then(res => res.json())
-      .then(res => {
+      .then((res) => {
         console.log(res);
-        setUsers(res)
+        setProduct(res)
+
       })
       .catch((err) => {
         console.log(err);
         setError(true)
 
-      }).finally(()=>{
-        setLoading(false)
+      }).finally(() => {
+        setLoading(false);
       })
   }, [])
   return (
     <>
-      <h1>Hello world</h1>
-      {loading && <h1>Loading...</h1>}
-      {error && <h1>Error occured.</h1>}
-      {users && users.map(item => {
-        return <h3 key={item.id}>{item.name}</h3>
-      })}
-{/* 
-      {loading ? <h1>Loading...</h1> : null}
-      {error ? <h1>Error occured.</h1> : null}
-      {users ? users.map(item => {
-        return <h3 key={item.id}>{item.name}</h3>
-      }) : null} */}
+    {loading && <h1>Loading...</h1>}
+    {error && <h1>error occured...</h1>}
+    {product && product.map(item => {
+      return <div key={item.id}>
 
+        <h1>{item.title}</h1>
+        <hr/>
+      </div>
+    })}
+    <div>App</div>
     </>
   )
 }
 
 export default App
-
-
-// falsy values in js
-
-// '' , 0 , null , undefined , false
