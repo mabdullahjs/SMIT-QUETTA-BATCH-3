d76694b9f02505985ed45c34da95439646e56f8e
Saylani-GenAIChatbot <highrated109@gmail.com>
2026-06-14 11:54:36 +0500

    form handling

--- 06-Backend/01-Introduction/index.js
+++ 06-Backend/01-Introduction/index.js
@@ -1,83 +1,98 @@
 import express from "express";
+import multer from "multer";
 
 const app = express();
-const port = 3000;
+const upload = multer()
+const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(upload.single('image'))
 
-const allTodos = [];
+// const allTodos = [];
 
-const checkMiddleware = (req, res, next) => {
-  console.log("check middleware");
-  next();
-};
+// const checkMiddleware = (req, res, next) => {
+//   console.log("check middleware");
+//   next();
+// };
 
-app.get("/", checkMiddleware, (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", checkMiddleware, (req, res) => {
+//   res.send("Hello World!");
+// });
 
 // create a new todo
-app.post("/todo", (req, res) => {
-  const { title } = req.body;
-  allTodos.push({
-    title,
-    id: Date.now(),
-  });
+// app.post("/todo", (req, res) => {
+//   const { title } = req.body;
+//   allTodos.push({
+//     title,
+//     id: Date.now(),
+//   });
 
-  res.status(201).json({
-    message: "New todo created",
-    todos: allTodos,
-  });
-});
+//   res.status(201).json({
+//     message: "New todo created",
+//     todos: allTodos,
+//   });
+// });
 
 // get all todo
 
-app.get("/todo", (req, res) => {
-  res.status(200).json({
-    todo: allTodos,
-  });
-});
+// app.get("/todo", (req, res) => {
+//   res.status(200).json({
+//     todo: allTodos,
+//   });
+// });
 
 // get single todo
 
-app.get("/todo/:id", (req, res) => {
-  const { id } = req.params;
-  const index = allTodos.findIndex((item) => item.id === +id);
-  console.log(index, id);
+// app.get("/todo/:id", (req, res) => {
+//   const { id } = req.params;
+//   const index = allTodos.findIndex((item) => item.id === +id);
+//   console.log(index, id);
 
-  if (index === -1) {
-    return res.status(404).json({
-      message: "todo not found",
-    });
-  }
+//   if (index === -1) {
+//     return res.status(404).json({
+//       message: "todo not found",
+//     });
+//   }
 
-  res.status(200).json({
-    todos: allTodos[index],
-  });
-});
+//   res.status(200).json({
+//     todos: allTodos[index],
+//   });
+// });
 
 // delete todo
 
-app.delete("/todo", (req, res) => {
-  const { id } = req.params;
-  const index = allTodos.findIndex((item) => item.id === +id);
+// app.delete("/todo", (req, res) => {
+//   const { id } = req.params;
+//   const index = allTodos.findIndex((item) => item.id === +id);
 
-  if (index === -1) {
-    return res.status(404).json({
-      message: "todo not found",
-    });
-  }
+//   if (index === -1) {
+//     return res.status(404).json({
+//       message: "todo not found",
+//     });
+//   }
 
-  allTodos.splice(index , 1);
-  res.status(204).json({
-    message: 'todo deleted',
-    todos: allTodos
-  })
-});
+//   allTodos.splice(index , 1);
+//   res.status(204).json({
+//     message: 'todo deleted',
+//     todos: allTodos
+//   })
+// });
 
 
 // edit todo
 
-app.put()
+// app.put()
+
+
+
+app.post("/form" ,upload.single("image") ,  (req , res)=>{
+  console.log(req.body);
+  console.log(req.file);
+  res.send("from received")
+  
+})
+
+
 
 
 app.listen(port, () => {
